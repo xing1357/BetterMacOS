e08467fad623464c55402140371d48a54a324169
Harvey Xing <xingharvey@gmail.com>
2021-03-11 15:50:17 -0600

    Update install.sh

--- Install/install.sh
+++ Install/install.sh
@@ -1,6 +1,6 @@
 unzip "Create New File.workflow.zip"
 unzip "Clear Cache.workflow.zip"
-cp -r ~/BetterFinder/"Create New File.workflow" ~/Library/Services
-cp -r ~/BetterFinder/"Clear Cache.workflow" ~/Library/Services
+cp -r ~"Create New File.workflow" ~/Library/Services
+cp -r ~"Clear Cache.workflow" ~/Library/Services
 Echo "BetterFinder Installed"
 Echo "Go to System Preferences > Keyboard > Shortcuts and scroll down to Clear Cache and Create New File. Click create shortcut and make a shortcut"
