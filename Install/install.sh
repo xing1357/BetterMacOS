55b4ab372ceafc29e10e901524e2baf28b89d50f
Harvey Xing <xingharvey@gmail.com>
2021-03-11 19:10:42 -0600

    Update install.sh

--- Install/install.sh
+++ Install/install.sh
@@ -4,3 +4,4 @@ cp -r ~"Create New File.workflow" ~/Library/Services
 cp -r ~"Clear Cache.workflow" ~/Library/Services
 Echo "BetterFinder Installed"
 Echo "Go to System Preferences > Keyboard > Shortcuts and scroll down to Clear Cache and Create New File. Click create shortcut and make a shortcut"
+Echo "Thank You For Choosing to use BetterFinder"
